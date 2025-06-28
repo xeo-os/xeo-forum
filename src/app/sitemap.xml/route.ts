ef3a39d84238594631a3907f1cad45dd52cf4a69
RavelloH <ravellohh@gmail.com>
2025-06-28 13:18:34 +0800

    Feat. 地图缓存

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -87,6 +87,7 @@ export async function GET() {
     return new Response(xml, {
         headers: {
             'Content-Type': 'application/xml',
+            'Cache-Control': 'public, max-age=3600, stale-while-revalidate=60',
         },
     });
 }
