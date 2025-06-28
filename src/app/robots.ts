ea8fb7412505fbbf0207623a8ee7e876afe5e928
RavelloH <ravellohh@gmail.com>
2025-06-28 12:53:14 +0800

    Feat. 更新robots.txt规则

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: '*',
             allow: '/',
-            disallow: ['/api', '/signup', '/signin', '/reset-password', '/verify'],
+            disallow: ['/signup', '/signin', '/reset-password', '/verify'],
         },
         sitemap: 'https://xeoos.net/sitemap.xml',
     };
