f6da817bb76607e2580a87e9123e1f21ed7d2ab9
RavelloH <ravellohh@gmail.com>
2026-03-08 15:58:56 +0800

    chore: 更改统计地址

--- src/components/umami-script.tsx
+++ src/components/umami-script.tsx
@@ -6,8 +6,7 @@ export function UmamiScript() {
   if (process.env.NODE_ENV === 'development') return null;
   return (
     <Script
-      src="https://analytics.ravelloh.top/script.js"
-      data-website-id="95e67860-7720-4f26-938c-649986984c8d"
+      src="https://insight.ravelloh.com/script.js?siteId=e56a11ef-e373-44e6-8ec9-50993bde1ef5"
       strategy="afterInteractive"
     />
   );
